a5526c88cd68a41ad1e49620a2328b06cfada537
JamNApple <bliu@utexas.edu>
2017-06-15 14:43:49 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -5,6 +5,10 @@ $(function() {
 		}
 	});
 
+	$("pre.html").snippet("html", {style:'matlab'});
+	$("pre.css").snippet("css", {style:'matlab'});
+	$("pre.javascript").snippet("javascript", {style:'matlab'});
+
 	$('#easyPaginate').easyPaginate({
 		paginateElement: 'img',
 		elementsPerPage: 3,
